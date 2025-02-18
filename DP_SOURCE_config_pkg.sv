cd4625716236807c93bff1854dc9a25353391c48
amira630 <a.amiraelkomy@outlook.com>
2025-02-18 02:56:04 +0200

    added DP_SOURCE_config

---
+++ DP_SOURCE_config_pkg.sv
@@ -0,0 +1,18 @@
+package DP_SOURCE_config_pkg;
+
+    import uvm_pkg::*;
+    `include "uvm_macros.svh"
+
+    class DP_SOURCE_config extends uvm_object;
+        `uvm_object_utils(alsu_config_obj)
+        
+        // Virtual Interfaces
+        virtual DP_SOURCE_if DP_TL_vif;
+        virtual DP_SOURCE_if DP_SINK_vif;
+
+        function new(string name = "DP_SOURCE_config");
+            super.new(name);
+        endfunction
+    endclass 
+
+endpackage
