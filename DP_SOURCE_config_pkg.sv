5aab6c665d4a811e544396b46ee9e831fa00d35e
amira630 <a.amiraelkomy@outlook.com>
2025-02-18 21:45:56 +0200

    updated DP_SOURCE_config

--- DP_SOURCE_config_pkg.sv
+++ DP_SOURCE_config_pkg.sv
@@ -7,8 +7,8 @@ package DP_SOURCE_config_pkg;
         `uvm_object_utils(alsu_config_obj)
         
         // Virtual Interfaces
-        virtual DP_SOURCE_if DP_TL_vif;
-        virtual DP_SOURCE_if DP_SINK_vif;
+        virtual DP_TL_if DP_TL_vif;
+        virtual DP_SINK_if DP_SINK_vif;
 
         function new(string name = "DP_SOURCE_config");
             super.new(name);
