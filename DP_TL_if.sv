df4e3a4237f1f3a79cc2adf79d3e247e7eab0c6e
amira630 <a.amiraelkomy@outlook.com>
2025-02-20 00:05:03 +0200

    added dummy DP_TL_if

--- DP_TL_if.sv
+++ DP_TL_if.sv
@@ -1,3 +1,11 @@
 interface DP_TL_if(clk);
-    // ... rest of the code
+    logic reset, valid_in, cin, valid_out, carry, zero;    
+    logic [3:0] a, b, alu; 
+    logic valid_out_ref, carry_ref, zero_ref;
+    logic [3:0] alu_ref;
+
+    modport DUT (
+        input clk, reset, valid_in, cin, a, b,
+        output valid_out, carry, zero, alu
+    );
 endinterface
