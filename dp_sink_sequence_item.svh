eed1d67941243a8b07f871e169111ef08e7c07c2
ayaeldesokey83 <yoka.eldesokey@gmail.com>
2025-02-23 20:54:25 +0200

    Updated Version

--- dp_sink_sequence_item.svh
+++ dp_sink_sequence_item.svh
@@ -8,7 +8,7 @@ class dp_sink_sequence_item extends uvm_sequence_item;
     endfunction //new()
 
     function string convert2string();
-        return $sformatf("%s name_1 = 0s%0s, name_2 = 0s%0s, name_3 = 0s%0s", super.convert2string(), name_1, name_2, name_3);
+        return $sformatf("%s name_1 = %0s, name_2 = %0s, name_3 = %0s", super.convert2string(), name_1, name_2, name_3);
     endfunction
     
 endclass //dp_sink_sequence_item extends superClass
