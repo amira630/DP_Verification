732ba37a19ed4396bd0b229b2ee5d88c92414b16
amira630 <a.amiraelkomy@outlook.com>
2025-03-14 18:59:30 +0200

    added comment

--- test_parameters_pkg.sv
+++ test_parameters_pkg.sv
@@ -3,7 +3,8 @@ package test_parameters_pkg;
     // Standard UVM import & include:
     import uvm_pkg::*;
     `include "uvm_macros.svh"
-
+    
+    // Includes:
     `include "test_parameters.svh"
     
 endpackage
