daa5ebb1775df057f2692727f51e55452a30dcc2
amira630 <a.amiraelkomy@outlook.com>
2025-03-16 13:57:08 +0200

    Added signals in TL interface

--- dp_tl_if.sv
+++ dp_tl_if.sv
@@ -9,7 +9,7 @@ interface dp_tl_if(input clk);
     logic [19:0] SPM_Address;
     logic [7:0]  SPM_Data, SPM_LEN, SPM_Reply_Data;
     logic [1:0]  SPM_CMD, SPM_Reply_ACK;
-    logic          SPM_Reply_ACK_VLD, SPM_Reply_Data_VLD, SPM_NATIVE_I2C, SPM_Transaction_VLD;
+    logic        SPM_Reply_ACK_VLD, SPM_Reply_Data_VLD, SPM_NATIVE_I2C, SPM_Transaction_VLD;
 
     ///////////////////////////////////////////////////////////////
     //////////////////// LINK POLICY MAKER ////////////////////////
@@ -18,13 +18,13 @@ interface dp_tl_if(input clk);
     logic [19:0] LPM_Address;
     logic [7:0]  LPM_Data, LPM_LEN, LPM_Reply_Data;
     logic [1:0]  LPM_CMD, LPM_Reply_ACK;
-    logic          LPM_Reply_ACK_VLD, LPM_Reply_Data_VLD, LPM_NATIVE_I2C,LPM_Transaction_VLD;
-    logic          HPD_Detect, HPD_IRQ;
+    logic        LPM_Reply_ACK_VLD, LPM_Reply_Data_VLD, LPM_NATIVE_I2C,LPM_Transaction_VLD;
+    logic        HPD_Detect, HPD_IRQ;
     ////////////////// LINK Training Signals //////////////////////
     logic [7:0] Link_LC_CR, Link_BW_CR, PRE, VTG, EQ_RD_Value, Lane_Align, MAX_VTG, EQ_Final_ADJ_BW;
     logic [3:0] CR_Done, EQ_CR_DN, Channel_EQ, Symbol_Lock;
     logic [1:0] EQ_Final_ADJ_LC;
-    logic         LPM_Start_CR, Driving_Param_VLD, EQ_Data_VLD, FSM_CR_Failed, EQ_Failed, EQ_LT_Pass;
+    logic       LPM_Start_CR, Driving_Param_VLD, EQ_Data_VLD, FSM_CR_Failed, EQ_Failed, EQ_LT_Pass;
 
     ///////////////////////////////////////////////////////////////
     //////////////////////// MODPORTS /////////////////////////////
